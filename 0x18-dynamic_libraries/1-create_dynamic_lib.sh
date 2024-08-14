ab3f703e4de8de108b37d70d3b5daf883ff27269
Ichapolly <pollykarani10@gmail.com>
2024-08-14 12:15:49 -0700

    nimerekebisha the script

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared -o liball.so *.o
+gcc -Wall -Wextra -Werror -pedantic -c -fPIC abs.c   isalpha.c  islower.c  memcpy.c  putchar.c  strcat.c  strcmp.c  strlen.c   strncpy.c  strspn.c atoi.c  isdigit.c  isupper.c  memset.c  puts.c     strchr.c  strcpy.c  strncat.c  strpbrk.c  strstr.c
+gcc -shared -o liball.so abs.o   isalpha.o  islower.o  memcpy.o  putchar.o  strcat.o  strcmp.o  strlen.o  strncpy.o  strspn.o
+atoi.o  isdigit.o  isupper.o  memset.o  puts.o  strchr.o  strcpy.o  strncat.o  strpbrk.o  strstr.o
